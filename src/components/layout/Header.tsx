f2ea4fa44e6f34f3be4a2e22fe7e326f18b7158c
Maribsorensen <148907991+Maribsorensen@users.noreply.github.com>
2025-05-13 15:51:12 +0200

    Feat: added basics, and setups

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,13 @@
+export function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>test1</li>
+          <li>test2</li>
+          <li>test3</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
